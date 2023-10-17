564da1e3446b2d71db7cc2d7d22e5573fe24a5fe
Stephen Equan <sequan@cannonschool.org>
2023-10-17 10:37:07 -0400

    calculator bot

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ class SimpleView(discord.ui.View):
     async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_message("Cancelling")
 
+  
+
 
 class TimeoutView(discord.ui.View):
-
+     
     foo = None
 
     async def disable_all_items(self):
@@ -36,14 +38,10 @@ class TimeoutView(discord.ui.View):
         await self.message.edit(view=self)   
         
 
-
-
-
     async def on_timeout(self) -> None:
         await self.message.channel.send("Timeout")
         await self.disable_all_items()
-
-
+    
     @discord.ui.button(label="Hello")
     async def hello(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_message("Hello World")
@@ -54,6 +52,64 @@ class TimeoutView(discord.ui.View):
     async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_message("Cancelling")
         self.foo = False
+        self.stop()
+
+class ButtonCalculator(discord.ui.View):
+       async def disable_all_items(self):
+        for item in self.children:
+            item.disabled = True
+
+        await self.message.edit(view=self)   
+        
+
+       async def on_timeout(self) -> None:
+        await self.message.channel.send("Timeout")
+        await self.disable_all_items()
+    #-1 is nothing 0 is add, 1 is subract, 2 is muultiply, and 3 is divide
+        operation = -1  
+
+@discord.ui.button(label="Add")
+async def add(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("Currently Adding. . .")
+        self.operation = 0
+        self.stop()    
+
+
+@discord.ui.button(label="Subtract")
+async def subtract(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("Currently Subtracting. . .")
+        self.operation = 1
+        self.stop()
+
+
+@discord.ui.button(label="Multiply")
+async def multiply(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("Currently Multiplying. . .")
+        self.operation = 2
+        self.stop()
+
+@discord.ui.button(label="Divide")
+async def divide(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("Currently Dividing. . .")
+        self.operation = 3
+        self.stop()
+
+
+
+
+
+
+
+
+   
+
+
+   
+
+    
+
+
+
 
 
  
@@ -109,14 +165,46 @@ async def button(ctx):
 
 
   
-     await ctx.send(view=view)
+     
 
      if view.foo is None:
          print("Timeout")
      elif view.foo == True:
          print("ok")
      else:
-         print("cancel")        
+         print("cancel")  
+
+@bot.command()
+async def buttonMath(ctx, arg1,arg2):
+    a = int(arg1)
+    b = int(arg2)
+
+    view =  ButtonCalculator(timeout = 30)
+
+    message = await ctx.send(view=view)
+
+    view.message = message
+
+    await view.wait()
+    await view.disable_all_items()
+    result = None
+
+    if view.operation == 0:
+        result = add(a, b)
+
+    elif view.operation == 1:
+        result = subtract(a, b)
+    elif view.operation == 2:
+        result = multiply(a, b)
+    elif view.operation == 3:
+        result = divide(a, b)    
+
+        
+
+            
+
+    
+              
 
 
 
@@ -125,7 +213,7 @@ async def button(ctx):
 
 
 
-#testibng
+#testing
 
      
 
